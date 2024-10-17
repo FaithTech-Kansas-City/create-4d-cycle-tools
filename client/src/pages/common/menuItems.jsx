d0a76287b5894d0b5030c003f8964385afd18b07
Collin Downey <143653096+Collin80r@users.noreply.github.com>
2024-10-16 22:16:30 -0500

    Add files via upload

---
+++ client/src/pages/common/menuItems.jsx
@@ -0,0 +1,24 @@
+// menuItems.js
+export const overviewItems = [
+  ];
+export const discoverItems = [
+    { label: 'Overview', path: '/discover/overview' },
+    { label: 'Projects', path: '/discover/projects' },
+    { label: 'Teams', path: '/discover/teams' },
+    { label: 'Problems', path: '/discover/problem' },
+    { label: 'Lament', path: '/discover/lament'}
+  ];
+export const discernItems = [
+  { label: 'Overview', path: '/discern/overview' },
+  { label: 'Brainstorm', path: '/discern/brainstorm' },
+  { label: 'Analysis', path: '/discern/analysis' },
+  { label: 'Timeline', path: '/discern/timeline' }
+  ];
+export const developItems = [
+  ];
+  
+  export const demonstrateItems = [
+    { label: 'Option 1', path: '/option1' },
+    { label: 'Option 2', path: '/option2' },
+  ];
+  
